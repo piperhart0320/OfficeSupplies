eaf433fe0165fb333496917b47c5bc52b924d249
root <root@DESKTOP-3LS8KCC.localdomain>
2024-02-01 09:50:24 -0800

    Created OfficeSuppliesTester.java

---
+++ OfficeSuppliesTester.java
@@ -0,0 +1,13 @@
+public class OfficeSuppliesTester 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to your office!");
+
+        Stapler myOffice = new Stapler();
+
+        System.out.println("Stapling papers...");
+
+        myOffice.staple();
+    }
+}
