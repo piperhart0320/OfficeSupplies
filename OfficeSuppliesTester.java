b4d47903f9d462cfcbf685fbe60f3095bac970ad
root <root@DESKTOP-3LS8KCC.localdomain>
2024-02-01 10:14:35 -0800

    oops forgot to do the add.Tester ready for Stapler/Papershredder.

--- OfficeSuppliesTester.java
+++ OfficeSuppliesTester.java
@@ -8,6 +8,37 @@ public class OfficeSuppliesTester
 
         System.out.println("Stapling papers...");
 
+        myOffice.staple(); //staples paper
+        myOffice.getAmount(); //checks how many staples are left
         myOffice.staple();
+        myOffice.refill(); //refills stapler
+        myOffice.staple();
+        myOffice.staple();
+        myOffice.getAmount();
+        System.out.println("");
+
+        PaperShredder Office = new PaperShredder();
+
+        System.out.println("Turning on papershredder..");
+
+        Office.turnOn(); 
+
+        System.out.println("Shredder is turned...(1 is on, 2 is off): " + Office.getStatus());
+
+        System.out.println("Shredding paper...");
+        Office.shred();
+        Office.shred();
+
+        System.out.println("Checking charge..");
+        System.out.println("Your charge is " + Office.getCharge());
+        
+        System.out.println("Recharging...");
+        Office.recharge();
+        System.out.println("Your charge is " + Office.getCharge());
+        
+        System.out.println("Turning off papershredder...");
+        Office.turnOff(); 
+        System.out.println("Shredder is turned...(1 is on, 2 is off): " + Office.getStatus());
+
     }
 }
--- Stapler.java
+++ Stapler.java
@@ -2,25 +2,27 @@ public class Stapler
 {
 
     private int staple_amount;
+    
 
     public Stapler()
     {      
         staple_amount = 210;
     }
 
-    public int getAmount()
+    public void getAmount()
     {
-        return staple_amount;
+        System.out.println("You have " + staple_amount + " left.");
     }
 
     public void refill()
     {
+        System.out.println("You have refilled the stapler! 210 staplers ready.");
         staple_amount = 210;
 
     }
 
     public void staple()
-    {
+    {   
         if (staple_amount < 0)
         {
             System.out.println("Wuh oh- no more staples!");
